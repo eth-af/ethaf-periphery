b9fb4dc78e35ed261ac60bc5afdfc4f1f59d3f46
wwHysenberg <wwhysenberg@protonmail.com>
2024-06-15 04:54:32 -0700

    rename position nft symbol

--- contracts/NonfungiblePositionManager.sol
+++ contracts/NonfungiblePositionManager.sol
@@ -72,7 +72,7 @@ contract NonfungiblePositionManager is
         address _factory,
         address _WETH9,
         address _tokenDescriptor_
-    ) ERC721Permit('ETH AF Positions NFT-V1', 'UNI-V3-POS', '1') PeripheryImmutableState(_factory, _WETH9) {
+    ) ERC721Permit('ETH AF Positions NFT-V1', 'ETH-AF-POS', '1') PeripheryImmutableState(_factory, _WETH9) {
         _tokenDescriptor = _tokenDescriptor_;
     }
 
